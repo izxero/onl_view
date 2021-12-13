884b41ca2c7189f192256886d455664eea3be5f0
savirusing <savirusing@hotmail.com>
2021-12-13 20:14:19 +0700

    upd complete but need to nil date

--- public/html/sql2excel/view.js
+++ public/html/sql2excel/view.js
@@ -4,114 +4,228 @@ webix.ready(function(){
     webix.ui({
         rows:[
             navBar,
-            sqlTable,
+            sql2excelTable,
         ]
     });
 });
 
-var keys_vals = {
-    REP_ID:"รหัสรายงาน",
-    LINE_NO:"บรรทัด",
-    CREATE_BY:"ผู้สร้าง",
-    CREATE_DATE:"วันที่สร้าง",
-    SQL_TEXT:"SQL Text",
-    COLUMN_HEADING1:"หัวคอลัมน์",
-    REP_NAME:"ชื่อรายงาน",
-    DOC_NO:"รหัสเอกสาร",
-}
-var sql2excel = genIdHeader(keys_vals);
+// var keys_vals = {
+//     REP_ID:"รหัสรายงาน",
+//     LINE_NO:"บรรทัด",
+//     CREATE_BY:"ผู้สร้าง",
+//     CREATE_DATE:"วันที่สร้าง",
+//     SQL_TEXT:"SQL Text",
+//     COLUMN_HEADING1:"หัวคอลัมน์",
+//     REP_NAME:"ชื่อรายงาน",
+//     DOC_NO:"รหัสเอกสาร",
+// }
+// var sql2excel = genIdHeader(keys_vals);
 
-function genIdHeader(obj){
-    let res = {}
-    for (const [key,value] of Object.entries(obj)){
-        let eachres = {}
-        eachres["id"] = key
-        eachres["header"] = value;
-        res[key] = eachres
-    }
-    return res;
-}
+// function genIdHeader(obj){
+//     let res = {}
+//     for (const [key,value] of Object.entries(obj)){
+//         let eachres = {}
+//         eachres["id"] = key
+//         eachres["header"] = value;
+//         res[key] = eachres
+//     }
+//     return res;
+// }
 
+
+// var navBar = {
+//     view:"toolbar",
+//     css:"pasteldarkgreen",
+//     cols:[
+//         createHeader("SQL To Excel",{css:"whitetext"}),
+//         {view:"button",type:"icon",icon:"fas fa-plus",tooltip:"เพิ่มใหม่",label:"เพิ่ม",width:130,css:"mybutton",click:testclick},
+//     ]
+// }
+
+// const theme = "pastellightgreen";
+// function getHeader(id){
+//     console.log(id);
+//     return "h";
+//     // return sql2excel[id].header
+// }
+// var sqlTable = {
+//     id:"sqlTable",
+//     view:"datatable",
+//     select:"row",
+//     css:"rows",
+//     url:onl_const.api+"get/sqlq/REP6410-0016",
+//     tooltip:true,
+//     columns:createColumns(sql2excel,{
+//         INDEX:{width:50,header:"ลำดับ"},
+//         // LINE_NO:{width:100},
+//         REP_NAME:{width:400,},
+//         SQL_TEXT:{width:400,fillspace:true},
+//     }),
+//     // columns:[
+//     //     {id:"INDEX",width:50,header:[{text:"No.",rowspan:2,css:theme+" textcenter"}],css:"textcenter"},
+//     //     // {id:"REP_NAME",width:400,header:[{text:"ชื่อรายงาน",css:theme},{content:"textFilter",css:theme}]},
+//     //     // {id:"SQL_TEXT",minWidth:250,fillspace:true,header:[{text:"SQL",css:theme},{content:"textFilter",css:theme}]},
+//     //     {width:400,id:sql2excel.REP_NAME.id,header:[{text:sql2excel.REP_NAME.header,css:theme},{content:"textFilter",css:theme}]},
+//     //     {width:250,id:sql2excel.SQL_TEXT.id,header:[{text:sql2excel.SQL_TEXT.header,css:theme},{content:"textFilter",css:theme}],fillspace:true},
+//     // ],
+//     on:{
+//         onItemDblClick:function(id){
+//             let current = this.getItem(id);
+//             console.log(current);
+//         },
+//         "data->onStoreUpdated":function(){
+//             this.data.each(function(obj, i){
+//                 obj.INDEX = i+1;
+//             });
+//         }
+//     }
+// }
+
+// function createColumns(fields,obj){
+//     let columns = [];
+//     for (const [key,prop] of Object.entries(obj)){
+//         let property = prop;
+//         //set property id
+//         if ((property.id == null)&&(property.id == "")){
+//             property.id = key;
+//         }
+//         //set property header
+//         if (fields[key] != null) {
+//             if (property.header != null){
+//                 x = 1; 
+//             }else{
+//                 property.header = fields[key].header;
+//             }
+//         }
+//         columns.push(property);
+//     }
+//     console.log(columns);
+//     return columns;
+// }
+
+// function refreshColumns(id_name){
+//     $$(id_name).clearAll();
+//     $$(id_name).config.columns = [];
+//     $$(id_name).refreshColumns();
+// }
+
+// function testclick(){
+//     console.log(sql2excel);
+//     // console.log(text);
+//     // console.log(sql2excel_ID);
+// }
 
 var navBar = {
     view:"toolbar",
     css:"pasteldarkgreen",
     cols:[
-        createHeader("SQL To Excel",{css:"whitetext"}),
-        {view:"button",type:"icon",icon:"fas fa-plus",tooltip:"เพิ่มใหม่",label:"เพิ่ม",width:130,css:"mybutton",click:testclick},
+        createHeader("SQL2Excel",{css:"whitetext"}),
+        {view:"button",css:"mybutton",label:"Add",width:80,click:function(){
+            $$("sql2excel_form").setValues({DOC_NO:"NEW"});
+            $$("sql2excel_window").show();
+        }},
     ]
 }
 
-const theme = "pastellightgreen";
-function getHeader(id){
-    console.log(id);
-    return "h";
-    // return sql2excel[id].header
-}
-var sqlTable = {
-    id:"sqlTable",
+var sql2excelTable = {
     view:"datatable",
-    select:"row",
+    id:"sql2excel_table",
+    url:"http://localhost:9001/api/112/get/sqlq/REP6410-0016",
     css:"rows",
-    url:onl_const.api+"get/sqlq/REP6410-0016",
-    tooltip:true,
-    columns:createColumns(sql2excel,{
-        INDEX:{width:50,header:"ลำดับ"},
-        // LINE_NO:{width:100},
-        REP_NAME:{width:400,},
-        SQL_TEXT:{width:400,fillspace:true},
-    }),
-    // columns:[
-    //     {id:"INDEX",width:50,header:[{text:"No.",rowspan:2,css:theme+" textcenter"}],css:"textcenter"},
-    //     // {id:"REP_NAME",width:400,header:[{text:"ชื่อรายงาน",css:theme},{content:"textFilter",css:theme}]},
-    //     // {id:"SQL_TEXT",minWidth:250,fillspace:true,header:[{text:"SQL",css:theme},{content:"textFilter",css:theme}]},
-    //     {width:400,id:sql2excel.REP_NAME.id,header:[{text:sql2excel.REP_NAME.header,css:theme},{content:"textFilter",css:theme}]},
-    //     {width:250,id:sql2excel.SQL_TEXT.id,header:[{text:sql2excel.SQL_TEXT.header,css:theme},{content:"textFilter",css:theme}],fillspace:true},
-    // ],
+    select:"row",
+    columns:[
+        {id:"DOC_NO",width:120,header:[{text:"DOC_NO"},{content:"textFilter"}]},
+        {id:"REP_NAME",width:300,header:[{text:"REP_NAME"},{content:"textFilter"}]},
+        {id:"SQL_TEXT",minWidth:300,fillspace:true,header:[{text:"SQL_TEXT"},{content:"textFilter"}]},
+        // {id:"CREATE_DATE",width:150,header:"Date"},
+    ],
     on:{
         onItemDblClick:function(id){
-            let current = this.getItem(id);
-            console.log(current);
-        },
-        "data->onStoreUpdated":function(){
-            this.data.each(function(obj, i){
-                obj.INDEX = i+1;
-            });
+            let current_data = this.getItem(id);
+            console.log(current_data);
+            $$("sql2excel_form").setValues(current_data);
+            $$("sql2excel_window").show();
         }
     }
 }
 
-function createColumns(fields,obj){
-    let columns = [];
-    for (const [key,prop] of Object.entries(obj)){
-        let property = prop;
-        //set property id
-        if ((property.id == null)&&(property.id == "")){
-            property.id = key;
-        }
-        //set property header
-        if (fields[key] != null) {
-            if (property.header != null){
-                x = 1; 
-            }else{
-                property.header = fields[key].header;
-            }
-        }
-        columns.push(property);
+webix.ui({
+    view:"window",
+    id:"sql2excel_window",
+    fullscreen:true,
+    position:"center",
+    modal:true,
+    borderless:true,
+    head:{
+        view:"toolbar",
+        css:"pasteldarkgreen",
+        cols:[
+            createHeader("Add/Edit",{css:"whitetext"}),
+            {view:"button",css:"mybutton",label:"Save",width:80,click:saveSql2Excel},
+            {view:"button",css:"mybutton",label:"Close",width:80,click:function(){
+                $$("sql2excel_window").hide();
+            }},
+        ]
+    },
+    body:{
+        rows:[
+            {
+                view:"form",
+                id:"sql2excel_form",
+                borderless:true,
+                rows:[
+                    {rows:[
+                        {view:"datepicker",name:"CREATE_DATE",format:"%d/%m/%Y",hidden:true},
+                        {view:"datepicker",name:"UPD_DATE",format:"%d/%m/%Y",hidden:true},
+                    ]},
+                    {cols:[
+                        {view:"text",labelWidth:80,labelAlign:"right",name:"REP_NAME",label:"ชื่อ SQL"},
+                        {view:"text",labelWidth:80,labelAlign:"right",name:"DOC_NO",label:"DOC_NO",width:240,readonly:true},
+                    ]},
+                    {view:"textarea",labelWidth:80,labelAlign:"right",name:"SQL_TEXT",label:"SQL"},
+                ]
+            },
+            {view:"resizer"},
+            {},
+        ]
     }
-    console.log(columns);
-    return columns;
+}).hide();
+
+function saveSql2Excel(){
+    let objData = formDataToJSON("sql2excel_form","DOC_NO");
+    if (objData.CREATE_DATE==null){
+        objData.CREATE_DATE = new Date();
+    }
+    objData.UPD_DATE = new Date();
+    console.log(objData.UPD_DATE);
+    let post = {
+        TABLE:"sql2excel",
+        CTRLNO:"sql2excel",
+        PREFIX:"REP6499",
+        DATA:JSON.stringify(objData),
+    }
+    webix.ajax().post("http://localhost:9001/api/112/cud/upd",post,function(text){
+        let res = JSON.parse(text);
+        console.log(res);
+        if (res.status=="complete"){
+            // if (objData.DOC_NO == "NEW"){
+            //     objData.DOC_NO = res.DOC_NO;
+            //     $$("sql2excel_table").add(objData);
+            // }
+            $$("sql2excel_form").setValues({DOC_NO:res.DOC_NO},true);
+            $$("sql2excel_table").clearAll();
+            $$("sql2excel_table").load("http://localhost:9001/api/112/get/sqlq/REP6410-0016");
+            webix.message("Saved");
+        }else{
+            webix.message("error while saving");
+        }
+    });
 }
 
-function refreshColumns(id_name){
-    $$(id_name).clearAll();
-    $$(id_name).config.columns = [];
-    $$(id_name).refreshColumns();
+function formDataToJSON(form_id,pk_key){
+    let original_data = $$(form_id).getValues();
+    let actual_data = JSON.parse(JSON.stringify(original_data));
+    delete actual_data.id;
+    actual_data.PK = pk_key;
+    return actual_data;
 }
-
-function testclick(){
-    console.log(sql2excel);
-    // console.log(text);
-    // console.log(sql2excel_ID);
-}
-
